01643e8e1f6e9c3b906538f9ad221cd3e8798d2f
gareth <gareth>
2006-11-08 06:30:59 +0000

    Modified to compile both interfaces

--- compile-interface.bat
+++ compile-interface.bat
@@ -3,7 +3,9 @@ REM Modify this file to suite your environment
 set XPIDL_EXE=c:\software\gecko-sdk\bin\xpidl.exe
 set XPIDL_INC=C:\software\gecko-sdk\idl
 
-set TPL_SRC=src/interface/nsIModifyheaders.idl
-set TPL_TRG=target/interface/nsIModifyheaders
+set TPL_SRC=src/interface
+set TPL_TRG=target/interface
 
-%XPIDL_EXE% -m typelib -v -I %XPIDL_INC% -w -o %TPL_TRG% %TPL_SRC%
+@echo on
+%XPIDL_EXE% -m typelib -v -I %XPIDL_INC% -w -o %TPL_TRG%/nsIModifyheaders %TPL_SRC%/nsIModifyheaders.idl
+%XPIDL_EXE% -m typelib -v -I %XPIDL_INC% -w -o %TPL_TRG%/mhIHeader %TPL_SRC%/mhIHeader.idl
