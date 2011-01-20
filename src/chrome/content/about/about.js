fed592f811c1348445906636a75e75aa49f4bf1b
Gareth Hunt <gareth-hunt@rocketmail.com>
2011-01-19 18:34:34 -0800

    Removed the old 'Help' and 'About' XUL pages as they are no longer required.

--- src/chrome/content/about/about.js
+++
@@ -1,8 +0,0 @@
-// Loads the extension home page in a new tab
-function modifyheaders_visitHomePage()
-{
-    const newTab = window.opener.getBrowser().addTab("http://www.garethhunt.com/");
-
-    window.opener.getBrowser().selectedTab = newTab;
-    window.close();
-}
